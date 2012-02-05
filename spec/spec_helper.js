9bf86ff2988af3eeb8a2ba5b5ea60cb1e3b2375a
Gregor Melhorn <gregor.melhorn@googlemail.com>
2012-02-05 13:43:15 +0100

    added callback style functions for attributes

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -3,6 +3,7 @@ TestModel = function TestModel(attributes) {
   this.name = attributes.name;
   this.real = attributes.real;
   this.dynamic = attributes.dynamic;
+  this.called = attributes.called;
 }
 TestModel.prototype.save = function(callback) {
   callback(null)
